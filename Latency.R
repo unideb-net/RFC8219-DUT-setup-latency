93c2a4d9f1aca9e6e5f8fb4c9725ef91487f11cb
unideb-net <93406707+unideb-net@users.noreply.github.com>
2024-05-21 19:28:42 +0200

    Latency.R

--- Latency.R
+++ Latency.R
@@ -1,22 +1,22 @@
-# Szükséges csomagok betöltése
+# Loading the necessary packages
 library(tidyverse)
 library(broom)
 
-# Mappák elérési útvonalai
+# Folder paths
 single_dut2_path <- "DUT2"
 single_dut3_path <- "DUT3"
 dual_dut_path <- "DUT2-DUT3"
 
-# Fájlnevek
+# File names
 files <- c("N1.csv", "N2.csv", "N4.csv", "N8.csv", "N16.csv", "N32.csv", "N64.csv", "N128.csv", "N256.csv")
 
-# Adatok beolvasása és mediánok számítása
+# Reading data and calculating medians
 read_and_calculate_median <- function(path, file) {
   df <- read_csv(file.path(path, file))
   median(df$Fwd_TL, na.rm = TRUE)
 }
 
-# Adatok összegyűjtése
+# Collecting data
 results <- tibble(
   Network = as.numeric(c(1, 2, 4, 8, 16, 32, 64, 128, 256)),
   DUT2 = sapply(files, read_and_calculate_median, path = single_dut2_path),
@@ -24,28 +24,28 @@ results <- tibble(
   DUT2_DUT3 = sapply(files, read_and_calculate_median, path = dual_dut_path)
 )
 
-# Késleltetések összege Single DUT esetén
+# Sum of latencies for Single DUT
 results <- results %>%
   mutate(Single_DUT_Sum = DUT2 + DUT3)
 
-# Elemzés és kiegészítő változók
+# Analysis and auxiliary variables
 results <- results %>%
   mutate(
-    Condition_1_2_4 = if_else(Network %in% c(1, 2, 4) & abs(Single_DUT_Sum - DUT2_DUT3) < 0.23 * Single_DUT_Sum, TRUE, FALSE),
+    Condition_1_2_4 = if_else(Network %in% c(1, 2, 4) & abs(Single_DUT_Sum - DUT2_DUT3) < 0.25 * Single_DUT_Sum, TRUE, FALSE),
     Condition_8_256 = if_else(Network >= 8 & DUT2_DUT3 > Single_DUT_Sum, TRUE, FALSE)
   )
 
-# Páros t-teszt alkalmazása
+# Use of a Paired T-test
 t_test_results <- t.test(results$DUT2_DUT3, results$Single_DUT_Sum, paired = TRUE)
 
-# Páros t-teszt eredményeinek kiírása
+# Pair T-test results
 print(t_test_results)
 
-# T-teszt konfidencia intervallum számítása
+# Calculation of T-test confidence interval
 ci <- t_test_results$conf.int
 mean_diff <- t_test_results$estimate
 
-# Vonaldiagram készítése
+# Creating a line graph
 line_plot <- ggplot(results, aes(x = factor(Network, levels = c(1, 2, 4, 8, 16, 32, 64, 128, 256)))) + 
   geom_line(aes(y = DUT2, color = "DUT2", group = 1)) +
   geom_line(aes(y = DUT3, color = "DUT3", group = 1)) +
@@ -65,7 +65,7 @@ line_plot <- ggplot(results, aes(x = factor(Network, levels = c(1, 2, 4, 8, 16, 
 
 print(line_plot)
 
-# Páros t-teszt eredményeinek vonaldiagramja
+# Line plot of Paired T-test results
 t_test_plot <- ggplot(results, aes(x = factor(Network, levels = c(1, 2, 4, 8, 16, 32, 64, 128, 256)))) +
   geom_point(aes(y = DUT2_DUT3 - Single_DUT_Sum), color = "black") +
   geom_errorbar(aes(ymin = mean_diff - ci[2], ymax = mean_diff - ci[1]), width = 0.2) +
@@ -78,12 +78,12 @@ t_test_plot <- ggplot(results, aes(x = factor(Network, levels = c(1, 2, 4, 8, 16
 
 print(t_test_plot)
 
-# Eredmények kiíratása
+# Printing results
 results_with_diff <- results %>%
   mutate(Difference = DUT2_DUT3 - Single_DUT_Sum)
 
 print(results_with_diff)
 
-# Adatok exportálása CSV fájlba
+# Export data to CSV file
 write_csv(results_with_diff, "results_with_diff.csv")
 
